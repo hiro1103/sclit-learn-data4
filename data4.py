291c1d39e7c69e521b7b89e52fc73ec4476787cf
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-13 13:07:06 +0900

    Add files via upload

--- data4.py
+++ data4.py
@@ -10,7 +10,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.base import clone
+from itertools import combinations
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sympy import subsets
+from sklearn.neighbors import KNeighborsClassifier
 # サンプルデータを作成
 csv_data = '''A,B,C,D
               1.0,2.0,3.0,4.0
@@ -213,4 +218,93 @@ plt.xscale('log')
 plt.legend(loc='upper left')
 ax.legend(loc='upper center', bbox_to_anchor=(
     1.38, 1.03), ncol=1, fancybox=True)
+plt.show()
+
+
+class SBS():
+
+    def __init__(self, estimator, k_features, scoring=accuracy_score,
+                 test_size=0.25, random_state=1) -> None:
+
+        # 特徴量を評価する指標
+        self.scoring = scoring
+        # 推定器
+        self.estimator = estimator
+        # 選択する特徴量の個数
+        self.k_featurs = k_features
+        # テストデータの割合
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        # 訓練データとテストデータに分割
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=self.test_size, random_state=self.random_state
+        )
+        # すべての特徴量の個数、列インデックス
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        # すべての特徴量を用いてスコアを算出
+        score = self._calc_score(X_train, y_train,
+                                 X_test, y_test, self.indices_)
+
+        self.scores_ = [score]
+        # 特徴量が指定した個数になるまで処理を繰り返す
+        while dim > self.k_featurs:
+            # 空のスコアリストを作成
+            scores = []
+            # 空の列インデックスリストを作成
+            subsets = []
+            # 特徴量の部分集合を表す列インデックスの組み合わせごとに処理を反復
+            for p in combinations(self.indices_, r=dim-1):
+                # スコアを算出して格納
+                score = self._calc_score(X_train, y_train, X_test, y_test, p)
+                scores.append(score)
+                # 特徴量の部分集合を表す列インデックスのリストを格納
+                subsets.append(p)
+
+            # 最良のスコアのインデックスを抽出
+            best = np.argmax(scores)
+            # 最良のスコアとなる列インデックスを抽出して格納
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            # 特徴量の個数を1つだけ減らして次のステップへ
+            dim -= 1
+            # スコアを格納
+            self.scores_.append(scores[best])
+        # 最後に格納したスコア
+        self.k_featurs = self.scores_[-1]
+        return self
+
+    def transform(self, X):
+        # 抽出した特徴量を返す
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        # 指定された列番号indicesの特徴量を抽出してモデルを適合
+        self.estimator.fit(X_train[:, indices], y_train)
+        # テストデータを用いてクラスラベルを予測
+        y_pred = self.estimator.predict(X_test[:, indices])
+        # 真のクラスラベルと予測値を用いてスコアを算出
+        score = self.scoring(y_test, y_pred)
+        return score
+
+
+# K最近傍法分類器のインスタンスを生成
+knn = KNeighborsClassifier(n_neighbors=5)
+# 逐次後退選択のインスタンスを生成（特徴量の個数が1になるまで特徴量を選択)
+sbs = SBS(knn, k_features=1)
+# 遂次後退選択を実行
+sbs.fit(X_train_std, y_train)
+
+# 特徴量の個数のリスト(13,12,.........1)
+k_feat = [len(k) for k in sbs.subsets_]
+# 横軸を特徴量の個数、縦軸をスコアとした折れ線グラフのプロット
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
 plt.show()
